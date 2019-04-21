67d42a7513c3f1580a85c59580b3bfa3486f1dca
Martín Díaz <mfdiaz1@miuandes.cl>
2019-04-21 19:40:26 -0400

    Red phase 4: Added test for number string with custom delimiter

--- test_string_add.py
+++ test_string_add.py
@@ -17,3 +17,9 @@ def test_add_string_default_delimiter_numbers():
     default_delimiter_numbers_string = "1,2,3,4"
     result = string_add.add(default_delimiter_numbers_string)
     assert result == 10, "Result should be 10"
+
+
+def test_add_string_custom_delimiter_numbers():
+    custom_delimiter_numbers_string = "2;9;5;12"
+    result = string_add.add(custom_delimiter_numbers_string, delimiter=';')
+    assert result == 30, "Result should be 30"
