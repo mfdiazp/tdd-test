4c19139531514e561f1c530f50a0296d22fa9e4f
Martín Díaz <mfdiaz1@miuandes.cl>
2019-04-21 19:03:52 -0400

    Red phase 2: Added test for number string with no delimiters

--- test_string_add.py
+++ test_string_add.py
@@ -5,3 +5,9 @@ def test_add_string_wrong_format():
     wrong_string = "asdfghj"
     result = string_add.add(wrong_string)
     assert result == -1, "Result should be -1"
+
+
+def test_add_string_no_delimiter_numbers():
+    no_delimiter_numbers_string = "1234"
+    result = string_add.add(no_delimiter_numbers_string)
+    assert result == 1234, "Result should be 1234"
