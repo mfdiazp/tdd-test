932b98c1cedc7ef911b88e162ca9ab86410984c7
Martín Díaz <mfdiaz1@miuandes.cl>
2019-04-21 19:26:36 -0400

    Green phase 3: Test for number string with default delimiter now passes

--- string_add.py
+++ string_add.py
@@ -1,4 +1,9 @@
 def add(numbers):
-    if numbers.isdigit():
-        return int(numbers)
-    return -1
+    separated_numbers = numbers.split(',')
+    numbers_sum = 0
+    for number in separated_numbers:
+        if number.isdigit():
+            numbers_sum += int(number)
+        else:
+            return -1
+    return numbers_sum
