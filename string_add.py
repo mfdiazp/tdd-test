fe7de0c83422e899258ec738596c051f1766a575
Martín Díaz <mfdiaz1@miuandes.cl>
2019-04-21 20:00:24 -0400

    Green phase 5: Test for number string which includes a negative number now passes

--- string_add.py
+++ string_add.py
@@ -2,8 +2,10 @@ def add(numbers, delimiter=','):
     separated_numbers = numbers.split(delimiter)
     numbers_sum = 0
     for number in separated_numbers:
-        if number.isdigit():
-            numbers_sum += int(number)
-        else:
+        try:
+            number_value = int(number)
+            if number_value >= 0:
+                numbers_sum += number_value
+        except ValueError:
             return -1
     return numbers_sum
